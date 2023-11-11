bc9161a36630fd1f7433974e02a1c514470c6989
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-11-11 12:16:55 +0500

    Added Project Files

---
+++ Module 1 - Components/2. Rendering Lists in React/examples/src/App.js
@@ -0,0 +1,15 @@
+// import MapMethod from "./Components/MapMethod";
+// import ListComponents from "./Components/ListComponents";
+import Keys from "./Components/Keys";
+
+function App() {
+  return (
+    // <MapMethod />
+
+    // <ListComponents />
+
+    <Keys />
+  );
+}
+
+export default App;
