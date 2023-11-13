5a7191878be39be28363a0b20a9364354a2cb84b
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-11-13 12:33:16 +0500

    Added Keys.js

---
+++ Module 1 - Components/2. Rendering Lists in React/examples/src/Components/Keys.js
@@ -0,0 +1,12 @@
+function Keys () {
+
+    return (
+        <ul>
+            <li key="Sprite">Sprite</li>
+            <li key="Pepsi">Pepsi</li>
+            <li key="Dew">Dew</li>
+        </ul>
+    )
+}
+
+export default Keys;
