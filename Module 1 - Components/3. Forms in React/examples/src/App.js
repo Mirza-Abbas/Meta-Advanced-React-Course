718462250b9082fc6265383cf9997e10eff732f7
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-11-14 14:13:26 +0500

    Added Project Files

---
+++ Module 1 - Components/3. Forms in React/examples/src/App.js
@@ -0,0 +1,18 @@
+// import UncontrolledInput from "./Components/UncontrolledInput";
+// import ControlledInput from "./Components/ControlledInput";
+// import FormComponent from "./Components/FormComponent";
+import ControlledForm from "./Components/ControlledForm";
+
+function App() {
+  return (
+    // <UncontrolledInput />
+
+    // <ControlledInput />
+
+    // <FormComponent />
+
+    <ControlledForm />
+  );
+}
+
+export default App;
