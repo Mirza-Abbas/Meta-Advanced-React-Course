b6a10a3fbf38e9f74a85c098b621302695ee38f1
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-11-19 12:34:27 +0500

    Added Project Files

---
+++ Module 2 - React Hooks and Custom Hooks/1. Getting Started with Hooks/examples/src/App.js
@@ -0,0 +1,28 @@
+// import UseState_Hook from "./Components/UseState_Hook";
+// import StateObject from "./Components/StateObject";
+// import ArrowFunctions from "./Components/ArrowFunctions";
+// import PureFunction from "./Components/PureFunction";
+// import ImpureFunction from "./Components/ImpureFunction";
+// import UseEffect_Hook from "./Components/UseEffect_Hook";
+import SideEffects from "./Components/SideEffects";
+
+function App() {
+
+  return (
+    // <UseState_Hook />
+
+    // <StateObject />
+
+    // <ArrowFunctions />
+
+    // <PureFunction year={2003}/>
+
+    // <ImpureFunction year={2003}/>
+
+    // <UseEffect_Hook />
+
+    <SideEffects />
+  )
+}
+
+export default App;
