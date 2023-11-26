97ced078d690377291fa2a2c15c26e507f60af0a
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-11-26 21:40:22 +0500

    Added Project Files

---
+++ Module 2 - React Hooks and Custom Hooks/3. Advanced Hooks/examples/src/App.js
@@ -0,0 +1,17 @@
+// import UseReducerHook from "./Components/UseReducerHook";
+// import UseRefHook from "./Components/UseRefHook";
+import CustomHook from "./Components/CustomHook";
+
+function App() {
+  
+  return (
+
+    // <UseReducerHook />
+
+    // <UseRefHook />
+
+    <CustomHook />
+  )
+}
+
+export default App;
