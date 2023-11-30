7d5a47ebbd6c57e098cbfae6e1a697a16eefebbf
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-11-30 22:11:45 +0500

    Add Project Files

---
+++ Module 3 - JSX and Testing/1. JSX Deep Dive/examples/src/App.js
@@ -0,0 +1,47 @@
+// import Specialization from "./Components/Specialization";
+// import ChildrenProp from "./Components/ChildrenProp";
+// import StringLiterals from "./Components/StringLiterals";
+// import JSXElements from "./Components/JSXElements";
+// import ReactFragments from "./Components/ReactFragments";
+// import Datatypes from "./Components/Datatypes";
+// import SpreadOperator from "./Components/SpreadOperator";
+// import CopyingObject from "./Components/CopyingObject";
+import MergingObject from "./Components/MergingObject";
+
+function App() {
+  
+  return (
+
+    // <Specialization />
+  
+    // <ChildrenProp color="blue">
+    //   <h1>Children</h1>
+    // </ChildrenProp>
+
+    // <StringLiterals />
+
+    // <JSXElements>
+    //   This is a nested Element
+    //   <ChildrenProp color="yellow">
+    //     Children
+    //   </ChildrenProp>
+    // </JSXElements>
+
+    // <ReactFragments>
+    //   <h1>
+    //     React Fragments Example
+    //   </h1>
+    // </ReactFragments>
+
+    // <Datatypes />
+
+    // <SpreadOperator />
+
+    // <CopyingObject />
+
+    <MergingObject />
+
+  );
+}
+
+export default App;
