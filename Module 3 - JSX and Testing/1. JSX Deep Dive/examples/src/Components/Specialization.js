36484d22a2551da53a37f5688d9d128b6fa8a24d
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-12-01 16:36:35 +0500

    Add Specialization.js

---
+++ Module 3 - JSX and Testing/1. JSX Deep Dive/examples/src/Components/Specialization.js
@@ -0,0 +1,12 @@
+import Containment from "./Containment";
+
+function Specialization () {
+    return (
+        <Containment color = "blue">
+            <h1>Hi!</h1>
+            <p>Bye!</p>
+        </Containment>
+    )
+}
+
+export default Specialization;
