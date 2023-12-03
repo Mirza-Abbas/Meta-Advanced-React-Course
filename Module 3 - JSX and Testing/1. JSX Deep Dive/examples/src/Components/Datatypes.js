3b964cea00b2cdaf6d7a1e9588ad4d71648e8b1e
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-12-04 00:03:42 +0500

    Add Datatypes.js

---
+++ Module 3 - JSX and Testing/1. JSX Deep Dive/examples/src/Components/Datatypes.js
@@ -0,0 +1,14 @@
+function Datatypes(){
+
+    return (
+        <>
+            These all will result in empty & same output:
+            <div>{false}</div>
+            <div>{null}</div>
+            <div>{undefined}</div>
+            <div>{true}</div>
+        </>
+    )
+}
+
+export default Datatypes;
